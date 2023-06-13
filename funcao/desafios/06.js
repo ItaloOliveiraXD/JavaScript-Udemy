a6c2329caecb07f8e21cc15cc00ddc6ca87aacc3
ítalo.oliveira <italooliveira-xd@hotmail.com>
2023-06-13 11:59:54 -0300

    Melhorando a resposta.

--- funcao/desafios/06.js
+++ funcao/desafios/06.js
@@ -1,8 +1,17 @@
 function simples (capital, taxaDeJuro ,tempo) {
-    let juro = capital * taxaDeJuro * tempo;
+    const juro = capital * taxaDeJuro * tempo;
     return capital += juro;
 }
 
 const aplicacao = simples(20000, 0.10, 12);
 
 console.log(aplicacao);
+
+function composto (capital, taxaDeJuro ,tempo) {
+    const montante = capital * Math.pow((1 + taxaDeJuro), tempo);
+
+    return montante.toFixed(2);
+}
+
+const aplicacao2 = composto(20000, 0.10, 12);
+console.log(aplicacao2);
