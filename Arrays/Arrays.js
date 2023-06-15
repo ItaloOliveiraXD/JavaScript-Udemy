055d63ba9c99ab6a0532b916413ea3c4afa51ebb
ítalo.oliveira <italooliveira-xd@hotmail.com>
2023-06-15 12:01:57 -0300

    Inicio da quarta sessão do curso Arrays.

---
+++ Arrays/Arrays.js
@@ -0,0 +1,32 @@
+console.log(typeof Array, typeof new Array, typeof []);
+
+let aprovados = new Array('Bia', 'Pedro', 'Clecio');
+
+aprovados = ['Italo', 'Sophia', 'Kelly'];
+console.log(aprovados[0]);
+console.log(aprovados[1]);
+console.log(aprovados[2]);
+
+aprovados[3] = 'Paulo';
+console.log(aprovados[3]);
+
+aprovados.push('Lia');
+console.log(aprovados[4]);
+
+console.log(aprovados);
+
+console.log('------------------------------------------------------------------');
+
+// Funcões para utilizar Arrays
+
+aprovados.length; // Saber o tamanho do array
+delete aprovados[1]; // Deletar um dado do array aparti do seu índice e deixa seu valor undefined.
+// Manipula o array 
+// primeiro param: indice que vai iniciar a manipulção.
+// segundo param: quantos itens vai deletar do array aparti do indice informado.
+// aparti do terceiro você informa o que vai ser adicionado no array.
+aprovados.splice(1, 1, 'Sophia', 'Yuri');
+
+aprovados.sort(); // Organiza o array em ordem alfabetica.
+
+console.log(aprovados);
